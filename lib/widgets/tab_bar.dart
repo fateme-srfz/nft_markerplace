63b348e7d0be24d5d08ae175aac0a350cccab28a
Fateme Sarafraz <fatemesrfz.dev@gmail.com>
2025-11-19 10:48:33 +0330

    add customtab for click

--- lib/widgets/tab_bar.dart
+++ lib/widgets/tab_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nft_markerplace/widgets/custom_tab.dart';
 
 class TabBarWidget extends StatelessWidget {
   const TabBarWidget({super.key});
@@ -7,70 +8,7 @@ class TabBarWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-      child: Row(
-        children: [
-          Expanded(
-            child: Container(
-              padding: const EdgeInsets.symmetric(vertical: 12),
-              decoration: BoxDecoration(
-                color: Colors.transparent,
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset(
-                    'assets/icons/rankingicon.png',
-                    width: 19,
-                    height: 19,
-                    color: Color(0xFFFFFFFF),
-                  ),
-                  const SizedBox(width: 5),
-                  Text(
-                    'Ranking',
-                    style: TextStyle(
-                      fontFamily: 'SFProDisplay',
-                      fontSize: 15,
-                      fontWeight: FontWeight.w700,
-                      color: Color(0xFFFFFFFF),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Expanded(
-            child: Container(
-              padding: const EdgeInsets.symmetric(vertical: 12),
-              decoration: BoxDecoration(
-                color: Colors.transparent,
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset(
-                    'assets/icons/activityicon.png',
-                    width: 19,
-                    height: 19,
-                    color: Color(0xFFFFFFFF),
-                  ),
-                  const SizedBox(width: 5),
-                  Text(
-                    'Activity',
-                    style: TextStyle(
-                      fontFamily: 'SFProDisplay',
-                      fontSize: 15,
-                      fontWeight: FontWeight.w700,
-                      color: Color(0xFFFFFFFF),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
+      child: CustomTabs(),
     );
   }
 }
